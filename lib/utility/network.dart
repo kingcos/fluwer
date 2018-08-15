49277e8d760e2366230247f8fc6c72995b8cd3d9
kingcos <2821836721v@gmail.com>
2018-08-15 23:11:20 +0800

    Add network util

---
+++ lib/utility/network.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class Network {
+  static Future<String> get(String url, { Map<String, String> params }) async {
+    if (params != null & params.isNotEmpty) {
+      StringBuffer sb = new StringBuffer("?");
+      params.forEach((key, value) {
+        sb.write("$key" + "=" + "$value" + "&");
+      });
+
+      String param = sb.toString();
+      url += param.substring(0, param.length - 1);
+    }
+
+    http.Response response = await http.get(url);
+    return response.body;
+  }
+}
