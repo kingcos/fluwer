6f0c3d73a6b35f09a495068bb25adff8a1d4ea49
kingcos <2821836721v@gmail.com>
2018-08-15 23:11:41 +0800

    Add JenkinsJob model for test

---
+++ lib/model/jenkins_job.dart
@@ -0,0 +1,16 @@
+class JenkinsJob {
+  final String name;
+  final String url;
+
+  JenkinsJob(this.name, this.url);
+
+  JenkinsJob.fromJson(Map<String, dynamic> json)
+            : name = json['name'],
+              url = json['url'];
+
+  Map<String, dynamic> toJSON() =>
+      {
+        'name': name,
+        'url': url,
+      };
+}
