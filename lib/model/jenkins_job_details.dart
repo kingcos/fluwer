3b55a3395dabeabf80ab7e1ba726654e3d7dda63
kingcos <2821836721v@gmail.com>
2018-08-20 00:04:26 +0800

    Add Jenkins job details model

---
+++ lib/model/jenkins_job_details.dart
@@ -0,0 +1,18 @@
+class JenkinsJobDetails {
+  final String name;
+  final String description;
+
+  JenkinsJobDetails({this.name, this.description});
+
+  JenkinsJobDetails.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        description = json['description'];
+
+  Map<String, dynamic> toJSON() => {'name': name, 'description': description};
+}
+
+class JenkinsJobBuild {
+  final String number;
+
+  JenkinsJobBuild({this.number});
+}
