fe9808df2630eeb4328726b413d9be0ada91bedb
kingcos <2821836721v@gmail.com>
2018-08-20 02:30:04 +0800

    Show Jenkins job build param list & test GitLab repo branches API

--- lib/page/jobs/job_details.dart
+++ lib/page/jobs/job_details.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluwer/model/gitlab_branch.dart';
 import 'package:fluwer/model/jenkins_job_details.dart';
+import 'package:fluwer/utility/gitlab.dart';
 import 'package:fluwer/utility/jenkins.dart';
 import 'package:fluwer/utility/network.dart';
 
@@ -20,12 +22,39 @@ class JobDetailsPage extends StatefulWidget {
 class JobDetailsPageState extends State<JobDetailsPage> {
   String jobName;
   JenkinsJobDetails _jobDetails;
+  int _selectedItemIndex = 0;
+  List _branches = new List<String>();
 
   JobDetailsPageState({Key key, this.jobName}) {
-    fetchJobDetails();
+    _fetchGitLabRepoBranches();
+    _fetchJobDetails();
   }
 
-  void fetchJobDetails({String branch, String packType, String email}) async {
+  void _fetchGitLabRepoBranches() async {
+    var repoID = await GitLab.fetchRepoID();
+    var url = await GitLab.fetchAPIHost() +
+        GitLab.API_PREFIX +
+        GitLab.API_PROJECTS +
+        repoID +
+        GitLab.API_REPO_BRANCHES +
+        "?page=1&per_page=100";
+    var headers = await GitLab.fetchRequestHeader();
+
+    var data = await Network.get(url: url, headers: headers);
+
+    if (data == null) {
+      return;
+    }
+
+    setState(() {
+      for (var branchJSON in json.decode(data)) {
+        print(GitLabBranch.fromJSON(branchJSON).name);
+        _branches.add(GitLabBranch.fromJSON(branchJSON).name);
+      }
+    });
+  }
+
+  void _fetchJobDetails({String branch, String packType, String email}) async {
     var apiHost = await Jenkins.fetchAPIHost();
     var headers = await Jenkins.fetchRequestHeader();
     var data = await Network.get(
@@ -42,13 +71,54 @@ class JobDetailsPageState extends State<JobDetailsPage> {
     setState(() {
       var jobDetailsJSON = json.decode(data);
 
-      _jobDetails = JenkinsJobDetails.fromJson(jobDetailsJSON);
+      _jobDetails = JenkinsJobDetails.fromJSON(jobDetailsJSON);
     });
+  }
+
+  Widget _buildPicker({String title, List<String> data}) {
+    if (data.length == 0) {
+      return new Center(child: const CupertinoActivityIndicator());
+    }
+
+    return new Container(
+      decoration: const BoxDecoration(
+        color: CupertinoColors.white,
+        border: const Border(
+          top: const BorderSide(color: const Color(0xFFBCBBC1), width: 0.0),
+          bottom: const BorderSide(color: const Color(0xFFBCBBC1), width: 0.0),
+        ),
+      ),
+      height: 44.0,
+      child: new Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new DefaultTextStyle(
+            style: const TextStyle(
+              letterSpacing: -0.24,
+              fontSize: 17.0,
+              color: CupertinoColors.black,
+            ),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(title),
+                new Text(
+                  data[_selectedItemIndex],
+                  style: const TextStyle(color: CupertinoColors.inactiveGray),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_jobDetails == null) {
+    if (_jobDetails == null || _branches.length == 9) {
       return new Scaffold(
         appBar: new AppBar(
           title: new Text(jobName),
@@ -58,18 +128,31 @@ class JobDetailsPageState extends State<JobDetailsPage> {
     }
 
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(jobName),
-      ),
-      body: new Column(
-        children: <Widget>[
-          new Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: new Text(_jobDetails.description,
-                style: new TextStyle(fontSize: 18.0)),
-          )
-        ],
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text(jobName),
+        ),
+        body: new DecoratedBox(
+          decoration: const BoxDecoration(color: const Color(0xFFEFEFF4)),
+          child: new ListView(
+            children: <Widget>[
+              new Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(_jobDetails.description,
+                      style: new TextStyle(fontSize: 16.0))),
+              new GestureDetector(
+                onTap: () async {},
+                child: _buildPicker(title: "test1", data: _branches),
+              ),
+              new GestureDetector(
+                onTap: () async {},
+                child: _buildPicker(title: "test2", data: _branches),
+              ),
+              new GestureDetector(
+                onTap: () async {},
+                child: _buildPicker(title: "test3", data: _branches),
+              )
+            ],
+          ),
+        ));
   }
 }
