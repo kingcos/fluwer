f1745ac9c05efd19522647da12d2d635133f4928
kingcos <2821836721v@gmail.com>
2018-08-19 18:31:26 +0800

    Add post request

--- lib/utility/network.dart
+++ lib/utility/network.dart
@@ -4,19 +4,31 @@ import 'package:http/http.dart' as http;
 
 class Network {
   // HTTP GET
-  static Future<String> get(String url,
-      {Map<String, String> params, Map<String, String> headers}) async {
+  static Future<String> get(
+      {String url,
+      Map<String, String> params,
+      Map<String, String> headers}) async {
     if (params != null && params.isNotEmpty) {
-      StringBuffer sb = new StringBuffer("?");
+      var sb = new StringBuffer("?");
       params.forEach((key, value) {
         sb.write("$key" + "=" + "$value" + "&");
       });
 
-      String param = sb.toString();
+      var param = sb.toString();
       url += param.substring(0, param.length - 1);
     }
 
     http.Response response = await http.get(url, headers: headers);
+    return response.body;
+  }
+
+  // HTTP POST
+  static Future<String> post(
+      {String url,
+      Map<String, String> headers,
+      Map<String, String> body}) async {
+    var response = await http.post(url, headers: headers, body: body);
+
     return response.body;
   }
 }
