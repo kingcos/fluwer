353041935e4013788cb8f1e7ea61be3ac0716685
kingcos <2821836721v@gmail.com>
2018-08-19 18:31:15 +0800

    Support external param name

--- lib/model/jenkins_job.dart
+++ lib/model/jenkins_job.dart
@@ -2,7 +2,7 @@ class JenkinsJob {
   final String name;
   final String url;
 
-  JenkinsJob(this.name, this.url);
+  JenkinsJob({this.name, this.url});
 
   JenkinsJob.fromJson(Map<String, dynamic> json)
       : name = json['name'],
