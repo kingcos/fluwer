924ee7ac180b1b8276e95b6b9546b427bc5153cc
kingcos <2821836721v@gmail.com>
2018-08-19 11:32:16 +0800

    Fetch Jenkins API host from local

--- lib/utility/jenkins.dart
+++ lib/utility/jenkins.dart
@@ -4,15 +4,23 @@ import 'dart:async';
 
 class Jenkins {
   // APIs
-  static const API_Host = "http://YOUR_JENKINS_HOST";
   static const API_Jobs = "/api/json";
 
   // Keys
-  static const String KEY_TOKEN = "KEY_JENKINS_TOKEN";
+  static const String KEY_HOST = "JENKINS_KEY_HOST";
+  static const String KEY_TOKEN = "JENKINS_KEY_TOKEN";
 
-  static Future<Map<String, String>> requestHeader() async {
+  static Future<String> fetchAPIHost() async {
+    var host = await Helper.getString(KEY_HOST);
+
+    return host;
+  }
+
+  static Future<Map<String, String>> fetchRequestHeader() async {
     var header = new Map<String, String>();
+
     header["Authorization"] = await Helper.getString(KEY_TOKEN);
+
     return header;
   }
 }
