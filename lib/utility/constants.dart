2187ff85ba07490264a46f6b99098d6c438a782e
kingcos <2821836721v@gmail.com>
2018-08-20 22:21:10 +0800

    Update constants

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   // Placeholders
-  static const NO_DATA_PLACEHOLDER = "NO DATA.";
+  static const NO_DATA_PLACEHOLDER = "NO DATA.\nPlease check your network or settings.";
+
+  // Keys
+  static const KEY_EMAIL_SUFFIX = "KEY_EMAIL_SUFFIX";
 }
