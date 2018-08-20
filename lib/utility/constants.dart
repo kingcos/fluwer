fd7e300360310a04e94a6058069a6c33494bb4c2
kingcos <2821836721v@gmail.com>
2018-08-21 00:14:01 +0800

    Formatted

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -1,6 +1,7 @@
 class Constants {
   // Placeholders
-  static const NO_DATA_PLACEHOLDER = "NO DATA.\nPlease check your network or settings.";
+  static const NO_DATA_PLACEHOLDER =
+      "NO DATA.\nPlease check your network or settings.";
 
   // Keys
   static const KEY_EMAIL_SUFFIX = "KEY_EMAIL_SUFFIX";
