ad3b33259ef9e56466324cfc886961b95be92f0f
kingcos <2821836721v@gmail.com>
2018-08-21 23:04:53 +0800

    Add more models about Jenkins build

--- lib/model/jenkins_build_param.dart
+++
@@ -1,3 +0,0 @@
-class JenkinsBuildParam {}
-
-class JenkinsBuildParamDefaultValue {}
--- lib/model/jenkins_job_details.dart
+++ lib/model/jenkins_job_details.dart
@@ -1,12 +1,39 @@
 class JenkinsJobDetails {
   final String name;
   final String description;
+  final List<JenkinsBuildParamDefinition> paramDefinitions;
 
-  JenkinsJobDetails({this.name, this.description});
+  JenkinsJobDetails({this.name, this.description, this.paramDefinitions});
 
   JenkinsJobDetails.fromJSON(Map<String, dynamic> json)
       : name = json['name'],
-        description = json['description'];
+        description = json['description'],
+        paramDefinitions = json['actions']['parameterDefinitions'];
+}
+
+class JenkinsBuildParamDefinition {
+  final String name;
+  final String description;
+  final String type;
+  final JenkinsBuildParamDefinitionDefaultValue defaultValue;
+
+  JenkinsBuildParamDefinition(
+      {this.name, this.description, this.type, this.defaultValue});
+
+  JenkinsBuildParamDefinition.fromJSON(Map<String, dynamic> json)
+      : name = json['name'],
+        description = json['description'],
+        type = json['type'],
+        defaultValue = json['actions']['parameterDefinitions'];
+}
+
+class JenkinsBuildParamDefinitionDefaultValue {
+  final dynamic value;
+
+  JenkinsBuildParamDefinitionDefaultValue({this.value});
+
+  JenkinsBuildParamDefinitionDefaultValue.fromJSON(Map<String, dynamic> json)
+      : value = json['value'];
 }
 
 class JenkinsJobBuild {
