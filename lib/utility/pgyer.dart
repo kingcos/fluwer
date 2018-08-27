c6b6f14ca93d0f8ae1f360f7dd2a7482b58a09af
kingcos <2821836721v@gmail.com>
2018-08-27 23:22:51 +0800

    Add pgyer utility

---
+++ lib/utility/pgyer.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:fluwer/utility/helper.dart';
+
+class Pgyer {
+  // APIs
+  static const API_PREFIX = "https://www.pgyer.com/apiv2/app/";
+  static const API_APP_ALL_BUILDS = "builds";
+
+  // Keys
+  static const KEY_APP = "PGYER_KEY_APP";
+  static const KEY_API = "PGYER_KEY_API";
+
+  static Future<String> fetchAppKey() async {
+    var host = await Helper.getString(KEY_APP);
+
+    return host;
+  }
+
+  static Future<String> fetchAPIKey() async {
+    var host = await Helper.getString(KEY_API);
+
+    return host;
+  }
+}
