b11a995f99e220a4a0e2a67a68b309033bb4848e
kingcos <2821836721v@gmail.com>
2018-08-28 23:40:21 +0800

    Add token key & request header

--- lib/utility/jira.dart
+++ lib/utility/jira.dart
@@ -9,12 +9,20 @@ class Jira {
 
   // Keys
   static const KEY_HOST = "JIRA_KEY_HOST";
+  static const KEY_TOKEN = "JIRA_KEY_TOKEN";
   static const KEY_FILTER_ID = "JIRA_KEY_FILTER_ID";
 
   static Future<String> fetchAPIHost() async {
     var host = await Helper.getString(KEY_HOST);
 
     return host;
+  }
+
+  static Future<Map<String, String>> fetchRequestHeader() async {
+    var header = new Map<String, String>();
+
+    header["Authorization"] = await Helper.getString(KEY_TOKEN);
+    return header;
   }
 
   static Future<String> fetchFilterID() async {
