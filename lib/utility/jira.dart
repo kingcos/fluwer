a2a64d1749ebea7ac13d8518d666926177ac77af
kingcos <2821836721v@gmail.com>
2018-08-28 23:08:14 +0800

    Add Jira APIs

---
+++ lib/utility/jira.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:fluwer/utility/helper.dart';
+
+class Jira {
+  // APIs
+  static const API_PREFIX = "/rest/api/2";
+  static const API_FILTER = "/filter/";
+
+  // Keys
+  static const KEY_HOST = "JIRA_KEY_HOST";
+  static const KEY_FILTER_ID = "JIRA_KEY_FILTER_ID";
+
+  static Future<String> fetchAPIHost() async {
+    var host = await Helper.getString(KEY_HOST);
+
+    return host;
+  }
+
+  static Future<String> fetchFilterID() async {
+    var id = await Helper.getString(KEY_FILTER_ID);
+
+    return id;
+  }
+}
