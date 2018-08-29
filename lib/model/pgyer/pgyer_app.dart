68c521e3571a5aeee66460beff531e4c44d38ddc
kingcos <2821836721v@gmail.com>
2018-08-29 13:04:52 +0800

    Add more properties

--- lib/model/pgyer/pgyer_app.dart
+++ lib/model/pgyer/pgyer_app.dart
@@ -1,33 +1,42 @@
 class PgyerApp {
   final String buildKey;
+  final String buildFileSize;
   final String buildName;
   final String buildPassword;
   final String buildVersion;
   final String buildVersionNo;
+  final String buildBuildVersion;
   final String buildUpdateDescription;
   final String buildIcon;
   final String buildQRCodeURL;
   final String buildCreated;
+  final int todayDownloadCount;
 
   PgyerApp(
       {this.buildKey,
+      this.buildFileSize,
       this.buildName,
       this.buildPassword,
       this.buildVersion,
       this.buildVersionNo,
+      this.buildBuildVersion,
       this.buildUpdateDescription,
       this.buildIcon,
       this.buildQRCodeURL,
-      this.buildCreated});
+      this.buildCreated,
+      this.todayDownloadCount});
 
   PgyerApp.fromJSON(Map<String, dynamic> json)
       : buildKey = json['buildKey'],
+        buildFileSize = json['buildFileSize'],
         buildName = json['buildName'],
         buildPassword = json['buildPassword'],
         buildVersion = json['buildVersion'],
         buildVersionNo = json['buildVersionNo'],
+        buildBuildVersion = json['buildBuildVersion'],
         buildUpdateDescription = json['buildUpdateDescription'],
         buildIcon = json['buildIcon'],
         buildQRCodeURL = json['buildQRCodeURL'],
-        buildCreated = json['buildCreated'];
+        buildCreated = json['buildCreated'],
+        todayDownloadCount = json['todayDownloadCount'];
 }
